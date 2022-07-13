f510113c520cb58a7139d96b0b85e2e21fb8ee37
h0gan1ee <lihaojun2003@outlook.com>
2022-07-13 14:52:35 +0800

    update

--- src/includes/script.js
+++ src/includes/script.js
@@ -4,3 +4,6 @@ globalThis.trans = () => {
   document.getElementById("logo").src = new URL('./assets/images/lug-pub-logo-alt.svg', import.meta.url);
   return '🏳️‍⚧️'
 }
+
+navigator.serviceWorker.register(
+  new URL('./sw.js', import.meta.url), { type: 'module' })
