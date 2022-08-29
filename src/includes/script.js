6efac029bec90913637fee0ad0832847c5e4be1a
h0gan1ee <lihaojun2003@outlook.com>
2022-08-29 13:20:08 +0800

    add: tg group link

--- src/includes/script.js
+++ src/includes/script.js
@@ -1,5 +1,8 @@
 // import 'normalize.css'
 
+console.info("Welcome to the pub!")
+console.info("Telegram Group Invite Link: TXT record of tg.lug.pub")
+
 globalThis.trans = () => {
   document.getElementById("logo").src = new URL('./assets/images/lug-pub-logo-alt.svg', import.meta.url);
   return '🏳️‍⚧️'
