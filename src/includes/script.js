4272cc2abe1752fc3f0462a13f7bd245c852d186
Takehana <cowpowermax@pm.me>
2024-04-29 10:54:31 +0800

    update

--- src/includes/script.js
+++ src/includes/script.js
@@ -1,12 +1,31 @@
 // import 'normalize.css'
 
-console.info("Welcome to the pub!")
-console.info("Check out TXT record of tg.osa.moe")
-
-globalThis.feelLucky = () => {
-  document.getElementById("logo").src = new URL('./assets/images/lug-pub-logo-alt.svg', import.meta.url);
-  return '🎲'
-}
+console.info('Welcome to the pub!')
+console.info('Check out TXT record of tg.osa.moe')
 
 navigator.serviceWorker.register(
   new URL('./sw.js', import.meta.url), { type: 'module' })
+
+let logoIndex = 0
+const $logos = document.querySelectorAll('img#logo')
+const [$prevButton, $nextButton] = document.querySelectorAll('.logo-view > button')
+
+const updateLogos = () => {
+  $logos.forEach((logo, index) => {
+    if (index === logoIndex) {
+      logo.classList.add('primary')
+    } else {
+      logo.classList.remove('primary')
+    }
+  })
+}
+
+$prevButton.addEventListener('click', (_) => {
+  logoIndex = (logoIndex - 1 + $logos.length) % $logos.length
+  updateLogos();
+})
+
+$nextButton.addEventListener('click', (_) => {
+  logoIndex = (logoIndex + 1) % $logos.length
+  updateLogos();
+})
